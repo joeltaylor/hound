cf7faf70c0eab6a7eb94f08e3f621ca0dbf1050f
Greg Lazarev <greg.lazarev@gmail.com>
2018-06-19 23:37:14 -0700

    Upgrade JS testing libraries

--- app/javascript/modules/Utils.js
+++ app/javascript/modules/Utils.js
@@ -1,5 +1,5 @@
 export function getCSRFfromHead() {
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'test') {
     return "csrf_token";
   } else {
     return document.querySelector("meta[name=csrf-token]").content;
